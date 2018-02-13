8e128698054cc7d20bb0ed664ef1203c43cc419d
icshih <icshih@mac.com>
2018-02-13 22:03:27 +0100

    open ivoa packages

--- ics.tap/src/main/java/module-info.java
+++ ics.tap/src/main/java/module-info.java
@@ -4,6 +4,9 @@ module ics.tap {
     requires java.xml.bind;
     exports ics.astro.tap;
     exports ics.astro.tap.utils;
-    opens net.ivoa.xml.vodataservice.v1;
+    opens net.ivoa.xml.stc.stc_v1_30;
     opens net.ivoa.xml.uws.v1;
+    opens net.ivoa.xml.vodataservice.v1;
+    opens net.ivoa.xml.voresource.v1;
+    opens net.ivoa.xml.votable.v1;
 }
